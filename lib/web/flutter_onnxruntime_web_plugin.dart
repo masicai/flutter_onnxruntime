4a9066391190f0a5c39c9b11358a15f7a7ab8e29
biendltb <biendltb@gmail.com>
2025-09-05 20:00:17 +0700

    chore: add license header to web plugin implementation

--- lib/web/flutter_onnxruntime_web_plugin.dart
+++ lib/web/flutter_onnxruntime_web_plugin.dart
@@ -1,3 +1,9 @@
+// Copyright (c) MASIC AI
+// All rights reserved.
+//
+// This source code is licensed under the license found in the
+// LICENSE file in the root directory of this source tree.
+
 import 'dart:async';
 import 'dart:js_interop';
 import 'dart:js_interop_unsafe';
